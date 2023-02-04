3600d3c3271c169a5b90c6d2cefcab27f8f34bcd
im-sparrow05 <108974446+im-sparrow05@users.noreply.github.com>
2023-02-04 15:13:49 +0530

    initial commit

---
+++ DSA-Supreme/Codes/Patterns/floyd's_triangle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+    cin>>n;
+    int k=1;
+    for(int i=0;i<n;i++){            
+        for(int j=1;j<=i+1;j++){
+            cout<<k<<" ";
+            k++;
+        }
+        cout<<endl;
+    }
+    }
